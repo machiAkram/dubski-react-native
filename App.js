1b1834cd19a6c1a23ee468feb9fbe77dcc87ba71
Akrem Guendouz <rak4558@gmail.com>
2022-09-21 10:24:49 +0100

    add menu for gameMode

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import { MyAppContainer } from './src/containers';
 import { AppNavigator } from './src/navigators';
 import { NavigationContainer } from '@react-navigation/native';
 import { myTheme } from './src/theme/theme';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const App = () => {
-  return <NavigationContainer theme={myTheme}>
-    <MyAppContainer>
-        <AppNavigator />
-    </MyAppContainer>
-  </NavigationContainer>
+  return (
+    <PaperProvider theme={myTheme}>
+      <NavigationContainer theme={myTheme}>
+        <MyAppContainer>
+          <AppNavigator />
+        </MyAppContainer>
+      </NavigationContainer>
+    </PaperProvider>
+  )
 }
 
 export default App;
--- src/components/GameMode.js
+++ src/components/GameMode.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Title } from '../components';
+import { Button, Menu } from 'react-native-paper';
+import { colors } from '../theme/colors';
 
 const GameMode = () => {
+  const [visible, setVisible] = useState(false);
+  const [gameMode, setGameMode] = useState(null);
+
+  const toggleMenu = () => {
+    setVisible(!visible);
+  };
+
   return (
     <View style={styles.container}>
       <Title>Game Mode</Title>
-
+      <Menu
+        visible={visible}
+        onDismiss={toggleMenu}
+        anchor={<Button style={styles.button} mode='outlined' textColor={colors.primaryRed} icon='arrow-down' contentStyle={{ flexDirection: 'row-reverse' }} onPress={toggleMenu}>Choose Game Mode</Button>}>
+        <Menu.Item onPress={() => { setGameMode('1v1'); setVisible(false); }} title="1v1" />
+        <Menu.Item onPress={() => { }} title="Item 2" />
+        <Menu.Item onPress={() => { }} title="Item 3" />
+      </Menu>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20,
+    marginTop: 25,
+  },
+  button: {
+    marginHorizontal: 10,
+    marginTop: 10,
+    borderColor: colors.primaryRed
   }
 });
 
